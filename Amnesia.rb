26451222e1a6dbf54d014586a278c1e458dc1548
Tammy Treit <ttreit@gmail.com>
2015-10-03 21:03:29 -0700

    Intro function correctly calls generic_decision function and passes one argument.

--- Amnesia.rb
+++ Amnesia.rb
@@ -19,14 +19,20 @@ def ph
 end
 
 def intro
+	hline_top
 	puts "As you gain consciousness you blink and look around."
 	puts "You are lying on the rough surface of a dock."
 	puts "You can smell the salt water and hear sea birds squalking"
-	puts "and your eyes adjust to the bright light."
+	puts "as your eyes adjust to the bright light."
 	puts
 	puts "You realize that you are naked."
+	hline_bottom
+	puts
+	### Send arguments to generic decision
+	generic_decision(4)
 end
 
+=begin
 def intro_decision
 	puts "1. Jump into the water."
 	puts "2. Stand up and stretch."
@@ -42,18 +48,28 @@ def intro_decision
 		puts "I would loop here but I haven't gotten to loops yet."
 	end
 	
-	# d_input(d_intro)
 	puts
 end
+=end
+
+### UNSTABLE CODE
+### Make decision more extensible
+
+def generic_decision(arg_possible)
+	### Replace intro_decision - 
+	### How many possibilities?
+	possible = arg_possible
+	puts "There are #{possible} decisions."
+	
+	
+end
+
 
 ### Main Body
 
-hline_top
-
 intro
-
-hline_bottom
 
 puts
 
-intro_decision
+### Rem'd out for generic_decision experiment
+# intro_decision
