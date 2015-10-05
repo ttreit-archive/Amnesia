be8d634162816d9358bd2098e030949105bd35e4
Tammy Treit <ttreit@gmail.com>
2015-10-04 17:51:47 -0700

    Stable build of Amnesia. Just need to add content.

--- Amnesia.rb
+++ Amnesia.rb
@@ -7,62 +7,83 @@ def prompt
 end
 
 def hline_top
-	puts "_________________________________________________________"
+	puts "____________________________________________________________________"
 end
 
 def hline_bottom
-	puts "---------------------------------------------------------"
+	puts "--------------------------------------------------------------------"
 end
 
 def ph
 	puts "PLACEHOLDER"
 end
 
+def error_invalid_choice
+	puts "Please choose one of the options above."
+	puts "I would loop here but I haven't gotten to loops yet."
+end
+
 def intro
 	hline_top
-	puts "As you gain consciousness you blink and look around."
-	puts "You are lying on the rough surface of a dock."
-	puts "You can smell the salt water and hear sea birds squalking"
-	puts "as your eyes adjust to the bright light."
-	puts
-	puts "You realize that you are naked."
+	puts """ 
+	As you gain consciousness you blink and look around.
+	You are lying on the rough surface of a dock.
+	You can smell the salt water and hear sea birds squalking.
+	Your eyes adjust to the bright light; 
+	you realize that you are naked.
+	"""
 	hline_bottom
 	puts
-	### Send arguments to generic decision
-	#generic_decision(4, "text array")
+	
 	intro_choices = ["1. Jump into the water.", "2. Stand up and stretch."]
 	choice = decision_list(intro_choices)
 	if choice == "1"
-		puts "test1"
+		into_water
 	elsif choice == "2"
-		puts "test2"
+		ph
 	else
-		puts "or..."
+		error_invalid_choice
 	end
 end ### End of intro function
 
-=begin
-def intro_decision
-	puts "1. Jump into the water."
-	puts "2. Stand up and stretch."
-	d_prompt
-	d_intro = gets.chomp
-	if d_intro == "1"
-		puts ph
-	elsif d_intro == "2"
-		puts ph
-		puts ph
-	else 
-		puts "Please choose one of the options above."
-		puts "I would loop here but I haven't gotten to loops yet."
-	end
-	
+def into_water
+	hline_top
 	puts
-end
-=end
+	puts """ 
+	You feel the red flush of emabarassment creep up your 
+	neck and cheeks. You jump into the water and turn from 
+	red to blue as the cold shock paralyzes you.
 
-### UNSTABLE CODE
-### Make decision more extensible
+	Sputtering you manage to tread water and look around. 
+
+	You see no one. The water is calm.
+	"""
+	hline_bottom
+	puts
+	into_water_choices = ["1. Swim to shore.", "2. Haul yourself back onto the dock.", 
+		"3. Tread Water"]
+	choice = decision_list(into_water_choices)
+	
+	if choice == "1"
+		swim_to_shore
+	elsif choice == "2"
+		get_on_dock
+	elsif choice == "3"
+		tread_water
+	else
+		error_invalid_choice
+	end
+end ### End of into_water function
+
+def swim_to_shore
+end
+
+def get_on_dock
+end
+
+def tread_water
+end
+
 
 def decision_list(arg_decisions)
 	
@@ -73,16 +94,7 @@ def decision_list(arg_decisions)
 	response = gets.chomp
 return response
 	
-	
-	
-	#myarray = arg_text_array
-	#puts "myarray = #{myarray}"
-	
-	### Tell function what to do for each decision
-	### first need to enumerate decisions
-	### then need to print option text for each decision
-	### then need to point each option to a function
-	
+
 end
 
 
@@ -90,7 +102,7 @@ end
 
 intro
 
+puts 
+puts "The End!"
 puts
 
-### Rem'd out for generic_decision experiment
-# intro_decision
